76e138897505bbd4481588178ec80f509bfc709f
Metreveli <metreveli2014@gmail.com>
2020-11-25 06:24:29 +0400

    added sticky nav and contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+
+
 // button scroll
 
 $('.js--scroll-to-story').click(function () {
@@ -16,26 +19,28 @@ $('.js--scroll-to-story').click(function () {
     $('html, body').animate({scrollTop: $('.js--section-home').offset().top}, 1000); 
  });
 
-
-//   /* Navigation scroll */
-//   $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//         if (target.length) {
-//           $('html,body').animate({
-//             scrollTop: target.offset().top
-//           }, 1000);
-//           return false;
-//         }
-//       }
-//     });
-//   });
+ $('.js--scroll-to-contact').click(function () {
+   $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000); 
+});
 
 
-  // Playlist 
+ /* For the sticky navigation */
+ $('.js--section-story').waypoint(function(direction) {
+   if (direction == "down") {
+       $('nav').addClass('sticky');
+   } else {
+       $('nav').removeClass('sticky');
+   }
+}, {
+ offset: '60px;'
+});
 
+
+
+
+
+});
+  
   
 
 
